7a51a1cdbc5f734bb6e0a02748644f678029be22
Srivaishnavi23 <83265272+Srivaishnavi23@users.noreply.github.com>
2023-11-22 19:16:30 +0530

    Update userDetails.js

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -12,21 +12,37 @@ function generateRandomIV() {
 }
 
 // Encrypt 
+// Modify the encryptData function
 function encryptData(data) {
-  const iv = generateRandomIV();
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
-  let encryptedData = cipher.update(data, 'utf-8', 'hex');
-  encryptedData += cipher.final('hex');
-  return { encryptedData, iv: iv.toString('hex') };
-}
+    try {
+      const iv = generateRandomIV();
+      const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey, 'hex'), iv);
+      let encryptedData = cipher.update(data, 'utf-8', 'hex');
+      encryptedData += cipher.final('hex');
+      console.log('Encryption successful');
+      return { encryptedData, iv: iv.toString('hex') };
+    } catch (error) {
+      console.error('Encryption error:', error);
+      throw error;
+    }
+  }
+  
 
 // Decrypt 
+// Modify the decryptData function
 function decryptData(encryptedData, iv) {
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), Buffer.from(iv, 'hex'));
-  let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
-  decryptedData += decipher.final('utf-8');
-  return decryptedData;
-}
+    try {
+      const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey, 'hex'), Buffer.from(iv, 'hex'));
+      let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
+      decryptedData += decipher.final('utf-8');
+      console.log('Decryption successful');
+      return decryptedData;
+    } catch (error) {
+      console.error('Decryption error:', error);
+      throw error;
+    }
+  }
+  
 
 exports.signUp = async (req, res, next) => {
   console.log('Ready To Signup');
@@ -57,32 +73,56 @@ function generateToken(id, username, ispremiumuser) {
     return jwt.sign({ UserId: id, username: username, ispremiumuser }, 'HiToken!');
 }
 
+
+
+
+
+
 exports.loginUser = async (req, res, next) => {
   try {
+    console.log('Login route reached');
     const { email, password } = req.body;
+    console.log('Email:', email);
+    console.log('Password:', password);
+
     const user = await User.findAll({ where: { email } });
+    console.log('User length:', user.length);
 
     if (user.length > 0) {
       bcrypt.compare(password, user[0].password, (err, match) => {
+        console.log('bcrypt.compare callback reached');
         if (match) {
+          console.log('Password matches');
           const decryptedUsername = decryptData(user[0].username, user[0].usernameIV);
-          const decryptedEmail = decryptData(user[0].email, user[0].emailIV);
+          console.log('Decrypted Username:', decryptedUsername);
+          // ... rest of the success logic
           return res.status(201).json({
             message: 'Login Successful!',
             token: generateToken(user[0].id, decryptedUsername, user[0].ispremiumuser)
           });
         } else {
+          console.log('Password does not match');
           return res.status(400).json({ message: 'wrong password' });
         }
       });
     } else {
+      console.log('User not found');
       return res.status(207).json({ message: 'User not found' });
     }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
+    console.log('Error:', error);
+    return res.status(500).json({ error: error });
   }
 };
+
+
+
+    
+  
+
+
+
+   
 
 exports.getUsers = async (req, res, next) => {
   console.log('Getting Users');
@@ -94,6 +134,7 @@ exports.getUsers = async (req, res, next) => {
     res.status(500).json({ error: error });
   }
 };
+
 
 
 
